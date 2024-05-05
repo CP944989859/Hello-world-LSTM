6eb1cd864ecd26cc08490dac83586fda3ca5833c
CP944989859 <43111977+CP944989859@users.noreply.github.com>
2024-05-05 21:16:17 +0800

    Update RNN.py

--- RNN.py
+++ RNN.py
@@ -10,23 +10,37 @@ import torch
 from torch.autograd import Variable
 import numpy as np
 import matplotlib.pyplot as plt
-# class RNN:
-#     def __init__(self,model)
-#     super(self,)
-#     self.model = model
-#     self.fc = torch.nn.Linear(self.model)
-#
-#     def forward(self, )
-#         #reserved as the attention module interface
-#         return
+
+class RNNmodel(torch.nn.Module):
+    def __init__(self,cell_class, num_layer, input_size, mid_dim, out_dim, batch_first = False):
+    # def __init__(self,LSTMcell, num_layer, input_size, mid_dim, out_dim, batch_first = False):
+        super(RNNmodel, self).__init__()
+        # self.rnn = torch.nn.LSTM(inp_dim, mid_dim, mid_layers,batch_first=batch)  # api
+        self.rnn = LSTM.LSTM(cell_class, num_layer, input_size, hidden_size).to(device)  # rnn
+        # self.rnn = LSTM.LSTM(LSTMcell, num_layer, input_size, hidden_size).to(device)  # rnn
+        self.reg = torch.nn.Sequential(
+            torch.nn.Linear(mid_dim, mid_dim),
+            torch.nn.Tanh(),
+            torch.nn.Linear(mid_dim, out_dim),
+        )  # regression
+        
+    def forward(self, x):
+        # y = self.rnn(x)[0]  # y, (h, c) = self.rnn(x), unless attention interface not used
+        y, _ = self.rnn(x)  # y, (h, c) = self.rnn(x), unless attention interface not used
+        batch_size, seq_len, hid_dim = y.shape
+        y = y.reshape(-1, hid_dim)
+        y = self.reg(y)
+        y = y.reshape(batch_size, seq_len, -1)
+        return y
+
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = 'torch implement')
-    parser.add_argument('--model_size', type=int, nargs = '+', default = [2,50])
-    parser.add_argument('--hidden_size', type=int, default = 10)
-    parser.add_argument('--num_layer', type=int, default = 2)
-    parser.add_argument('--epochs', type=int, default = 10)
+    parser.add_argument('--model_size', type=int, nargs = '+', default = [2,10])  # num_layer * hidden_size
+    # parser.add_argument('--hidden_size', type=int, default = 10)
+    # parser.add_argument('--num_layer', type=int, default = 2)
+    parser.add_argument('--epochs', type=int, default = 100)
     parser.add_argument('--train_dir', type = str, default = './logs/')
     FLAGS, unparsed = parser.parse_known_args()
 
@@ -44,17 +58,16 @@ if __name__ == '__main__':
 
     input_size = 3
     batch_size = 32
-    # time_step = 2
     time_step = 4
     output_size = 1
 
-    num_layer = FLAGS.num_layer
-    hidden_size = FLAGS.hidden_size
+    num_layer = FLAGS.model_size[0]
+    hidden_size = FLAGS.model_size[1]
     epoch = FLAGS.epochs
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    net = LSTM.LSTM(LSTM.LSTMcell, num_layer, input_size, hidden_size).to(device)
-    print("line init....i")
+    # net = LSTM.LSTM(LSTM.LSTMcell, num_layer, input_size, hidden_size).to(device)
+    net = RNNmodel(LSTM.LSTMcell, num_layer, input_size, hidden_size, output_size).to(device)
     for param in net.state_dict():
         print("net.parameters()  ",param, net.state_dict()[param].shape)
         # print("net.parameters()  ",param)
@@ -63,7 +76,7 @@ if __name__ == '__main__':
     optimizer=torch.optim.Adam(net.parameters(),lr=1e-2)
 
     data = util.load_data()
-    print("data.shape:  ",data.shape)
+    print("Simulink Database.shape:  ",data.shape)
     train_size = int(len(data) * 0.75)
     data_sample = np.zeros((train_size - time_step + 1, time_step, input_size))
     label_sample = np.zeros((train_size - time_step + 1, time_step, output_size))
@@ -73,14 +86,12 @@ if __name__ == '__main__':
 
     for i in range(epoch):
         for j in range(int((train_size - time_step + 1) / batch_size)):
-
             train_X = data_sample[j * batch_size:(j + 1) * batch_size, :, :]
             train_Y = label_sample[j * batch_size:(j + 1) * batch_size, :, :]
             var_x = torch.tensor(train_X, dtype=torch.float32, device=device)
             var_y = torch.tensor(train_Y, dtype=torch.float32, device=device)
-            print("input.shape: ", var_x.shape)
             out = net(var_x)
-            print("Step j: ", j)
+            # print("Epochs:  ", i,  ", Step  :  ",  j,    var_y.shape, len(out), out.shape)
             loss = criterion(out, var_y)
             # loss = criterion(out[:,-1,:], var_y[:,-1,:])
             optimizer.zero_grad()
@@ -97,8 +108,7 @@ if __name__ == '__main__':
         optimizer.step()
         if i % 2 == 0:
             print('Epoch: {:4}, Loss: {:.5f}'.format(i, loss.item()))
-
-
+            
     net=net.eval()
     test_X=data[train_size:,:]
     test_Y=data[train_size+time_step:,0:1:]
@@ -114,18 +124,11 @@ if __name__ == '__main__':
     diff=test_y-test_Y
     l1_loss=np.mean(np.abs(diff))
     l2_loss=np.mean(diff**2)
-    print("L1:{:.3f}    L2:{:.3f}".format(l1_loss,l2_loss))
+    print("Eval :  L1:{:.3f}    L2:{:.3f}".format(l1_loss,l2_loss))
     plt.plot(test_y, 'r', label='pred')
     plt.plot(test_Y, 'b', label='real', alpha=0.3)
+    plt.legend()
+    plt.title(" View of casual sequence comparison")
+    plt.show()
 
 
-    a=np.array([1,2,3])
-    b=np.array([2,2,4])
-
-    a=a.reshape(-1,1)
-    b=b.reshape(-1,1)
-
-    np.corrcoef(a,b)
-    print("Line 187")
-    a.reshape((-1))
-    print("..........")
